59f80fd86d9efb5420319ede8f09236baac1dce8
lostsky3000 <296821855@qq.com>
2018-06-28 19:26:59 +0800

    update

--- start.bat
+++ start.bat
@@ -2,6 +2,21 @@
 
 echo %JAVA_HOME%
 
-java -jar js-server-page.jar %cd%
+set dir=%cd%
+set libDir=%cd%\lib
+
+set jarStr=
+
+cd %libDir% 
+for /r . %%i in (*.jar) do ( 
+	rem echo %%~nxi
+	set "jarStr=%jarStr%lib/%%~nxi;"
+)
+
+cd ..
+
+echo ExtJars=%jarStr%
+
+java -Xbootclasspath/a:%jarStr% -jar js-server-page.jar %dir%
 
 pause
--- start.sh
+++ start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-curDir=`pwd`
+
+dir=`pwd`
 #echo $curDir
-java -jar js-server-page.jar $curDir
+
+libDir=$dir/lib
+
+#echo $libDir
+
+jarStr=
+
+for file in $libDir/*.jar
+do
+if [ -f "$file" ]; then
+  #echo $file
+  jarStr=${jarStr}${file}":"
+fi
+done
+
+echo ExtJars=$jarStr
+
+java -Xbootclasspath/a:$jarStr -jar js-server-page.jar $dir
+
